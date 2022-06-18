01f87fc704d44a8cbc9aeb719c7a6f66da918757
Kienquan <hotrankienquan144@gmail.com>
2022-06-18 17:07:10 +0700

    config vercel

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 const PORT = 1000;
 
-app.use("/api/product", product);
+app.use("/", product);
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
 });
